e694b8e2eb7851a0a2362562b7368ab686fb9d12
Jorge H. P. Garcia <j.hrique@hotmail.com>
2015-03-01 00:05:14 -0300

    Função de audio para acessibilidade restaurada

--- js/funcoes.js
+++ js/funcoes.js
@@ -23,13 +23,28 @@ function switcher(opcao, nomeSwitcher){
 	document.getElementById(nomeSwitcher).whichChoice = opcao;
 }
 
-function showText(text){
-	var vet = text.split("."); var text2="";
+function showText(text, arq){
+	var vet = text.split(".");
+	var text2="";
 	for (var i in vet) {
  		text2 += vet[i] + ".</p><p>";
 	}
 	text2 = "<p>"+ text2 +"</p>";
-	document.getElementById("info").innerHTML = text2;
+
+	var info = document.getElementById("info");
+	if(text2 != info.innerHTML){
+		info.innerHTML = text2;
+		playAudio(arq);
+	}
+	//elem.style.cursor = "help";
+}
+
+function playAudio(arq){
+	var audioSrc = document.getElementById("audio");
+	var audioControl = document.getElementById("audioControl");
+	audioSrc.src = arq;
+	audioControl.load();
+	setTimeout(function(){ audioControl.play() }, 1000);
 }
 
 function fullscreen (element) {
