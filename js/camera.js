5aba4512f26b5a402a2db3985983ab349ab4b81c
Jorge H. P. Garcia <j.hrique@hotmail.com>
2015-04-03 21:30:14 -0300

    Câmera arrumada

--- js/camera.js
+++ js/camera.js
@@ -1,11 +1,12 @@
 
 
 /// Camera start ------------------------------------------
-
-var video = document.querySelector("#vid");
-var canvas;
-var ctx;
-var localMediaStream;
+window.onload =function(){
+	var video = document.querySelector("#vid");
+	var canvas;
+	var ctx;
+	var localMediaStream;
+}
 
 var onCameraFail = function (e) {
    console.log('Camera did not work.', e);
@@ -29,18 +30,20 @@ function snapshot2() {
 }
 
 function start() { 
+	//var video = document.querySelector("#vid");
 
 	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 	window.URL = window.URL || window.webkitURL;
+
 	navigator.getUserMedia({video:true}, function (stream) {
 	   video.src = window.URL.createObjectURL(stream);
 	   localMediaStream = stream;
 	}, onCameraFail);
 
-	navigator.getMedia({ video: true }, function (stream) {
-		video.src = window.URL.createObjectURL(stream);
-		localMediaStream = stream;
-	}, userMediaError());
+	// navigator.getMedia({ video: true }, function (stream) {
+	// 	video.src = window.URL.createObjectURL(stream);
+	// 	localMediaStream = stream;
+	// }, userMediaError());
 
 	if (!!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia)) {
 		video = document.querySelector("#vid");	//Elemento video
